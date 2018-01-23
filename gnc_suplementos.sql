b5aec44c033204aaa1d98085d50d052268b22d74
suriel1350 <kato_1350@hotmail.com>
2018-01-23 16:14:56 -0600

    base datos gnc

---
+++ gnc_suplementos.sql
@@ -0,0 +1,406 @@
+-- MySQL dump 10.13  Distrib 5.7.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: gnc_suplementos
+-- ------------------------------------------------------
+-- Server version	5.7.20-0ubuntu0.17.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) DEFAULT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `descripcion` varchar(256) DEFAULT NULL,
+  `condicion` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `idcliente` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `direccion` varchar(256) DEFAULT NULL,
+  `telefono` int(20) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `estado` varchar(30) DEFAULT NULL,
+  `pais` varchar(30) DEFAULT NULL,
+  `condicion` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_venta`
+--
+
+DROP TABLE IF EXISTS `detalle_venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `detalle_venta` (
+  `iddetalleventa` int(11) DEFAULT NULL,
+  `idventa` int(11) DEFAULT NULL,
+  `idproducto` int(11) DEFAULT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `precio_venta` decimal(11,3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_venta`
+--
+
+LOCK TABLES `detalle_venta` WRITE;
+/*!40000 ALTER TABLE `detalle_venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalle_venta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `devoluciones`
+--
+
+DROP TABLE IF EXISTS `devoluciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `devoluciones` (
+  `iddevoluciones` int(11) DEFAULT NULL,
+  `idproducto` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `idproveedor` int(11) DEFAULT NULL,
+  `razones` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `devoluciones`
+--
+
+LOCK TABLES `devoluciones` WRITE;
+/*!40000 ALTER TABLE `devoluciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `devoluciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empleado` (
+  `idempleado` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `rfc` varchar(50) DEFAULT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `estado` varchar(30) DEFAULT NULL,
+  `pais` varchar(30) DEFAULT NULL,
+  `imagen` varchar(30) DEFAULT NULL,
+  `condicion` tinyint(1) DEFAULT NULL,
+  `fecha_ingreso` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleado`
+--
+
+LOCK TABLES `empleado` WRITE;
+/*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `facturacion`
+--
+
+DROP TABLE IF EXISTS `facturacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `facturacion` (
+  `idfacturacion` int(11) DEFAULT NULL,
+  `idventa` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idempleado` int(11) DEFAULT NULL,
+  `total` decimal(11,3) DEFAULT NULL,
+  `fecha_facturacion` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `facturacion`
+--
+
+LOCK TABLES `facturacion` WRITE;
+/*!40000 ALTER TABLE `facturacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `facturacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `membresia`
+--
+
+DROP TABLE IF EXISTS `membresia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `membresia` (
+  `idmembresia` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `fecha_inicio` date DEFAULT NULL,
+  `fecha_final` date DEFAULT NULL,
+  `tipo_membresia` varchar(100) DEFAULT NULL,
+  `beneficios` varchar(100) DEFAULT NULL,
+  `foto_cliente` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `membresia`
+--
+
+LOCK TABLES `membresia` WRITE;
+/*!40000 ALTER TABLE `membresia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `membresia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pedido`
+--
+
+DROP TABLE IF EXISTS `pedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedido` (
+  `idpedido` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idproducto` int(11) DEFAULT NULL,
+  `num_pedido` int(11) DEFAULT NULL,
+  `fecha_pedido` datetime DEFAULT NULL,
+  `condicion` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido`
+--
+
+LOCK TABLES `pedido` WRITE;
+/*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producto` (
+  `idproducto` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `idcategoria` int(11) DEFAULT NULL,
+  `idproveedor` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `precio` decimal(11,3) DEFAULT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(256) DEFAULT NULL,
+  `imagen` varchar(100) DEFAULT NULL,
+  `condicion` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedor`
+--
+
+DROP TABLE IF EXISTS `proveedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `proveedor` (
+  `idproveedor` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `idproducto` int(11) DEFAULT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `direccion` varchar(30) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `estado` varchar(30) DEFAULT NULL,
+  `pais` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedor`
+--
+
+LOCK TABLES `proveedor` WRITE;
+/*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sucursal`
+--
+
+DROP TABLE IF EXISTS `sucursal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sucursal` (
+  `idsucursal` int(11) DEFAULT NULL,
+  `idempleado` int(11) DEFAULT NULL,
+  `idproveedor` int(11) DEFAULT NULL,
+  `idproducto` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idventa` int(11) DEFAULT NULL,
+  `ubicacion` varchar(100) DEFAULT NULL,
+  `estado` varchar(100) DEFAULT NULL,
+  `pais` varchar(100) DEFAULT NULL,
+  `num_sucursal` int(11) DEFAULT NULL,
+  `condicion` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sucursal`
+--
+
+LOCK TABLES `sucursal` WRITE;
+/*!40000 ALTER TABLE `sucursal` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sucursal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sueldo`
+--
+
+DROP TABLE IF EXISTS `sueldo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sueldo` (
+  `idsueldo` int(11) DEFAULT NULL,
+  `idempleado` int(11) DEFAULT NULL,
+  `cantidad` decimal(11,3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sueldo`
+--
+
+LOCK TABLES `sueldo` WRITE;
+/*!40000 ALTER TABLE `sueldo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sueldo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turno`
+--
+
+DROP TABLE IF EXISTS `turno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `turno` (
+  `idturno` int(11) DEFAULT NULL,
+  `idempleado` int(11) DEFAULT NULL,
+  `nombre_turno` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turno`
+--
+
+LOCK TABLES `turno` WRITE;
+/*!40000 ALTER TABLE `turno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `turno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `venta` (
+  `idventa` int(11) DEFAULT NULL,
+  `idcliente` int(11) DEFAULT NULL,
+  `idsucursal` int(11) DEFAULT NULL,
+  `idempleado` int(11) DEFAULT NULL,
+  `fecha_hora` datetime DEFAULT NULL,
+  `total_venta` decimal(11,3) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venta`
+--
+
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-23 16:11:42
